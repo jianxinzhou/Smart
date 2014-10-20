10f44d71872515119860267a59e4116ba62a70c9
KrisChou <zhoujx0219@163.com>
2014-10-20 21:52:51 +0800

    update

--- include/MyLock.h
+++ include/MyLock.h
@@ -4,34 +4,37 @@
 #include <iostream>
 class MyLock
 {
-	public:
-		MyLock()
-		{
+    public:
+        MyLock()
+        {
             // 初始化成功返回0
-			if( pthread_mutex_init(&m_mutex, NULL) )
-			{
-			 	std::cout << __DATE__  << " " << __TIME__ << " " 
+            if( pthread_mutex_init(&m_mutex, NULL) )
+            {
+                std::cout << __DATE__  << " " << __TIME__ << " " 
                           << __FILE__  << " " << __LINE__ << ":"
                           << "pthread_mutex_init failed!" << std::endl;
-				exit (-1); 
-			}
-		}
-		void lock()
+                exit (-1); 
+            }
+        }
+		
+        void lock()
 		{
 			pthread_mutex_lock(&m_mutex) ;
 		}
-		void unlock()
+		
+        void unlock()
 		{
 			pthread_mutex_unlock(&m_mutex) ;
 		}
-		~MyLock()
+		
+        ~MyLock()
 		{
 			pthread_mutex_destroy(&m_mutex) ;
 		}
 	
     private:
-	    pthread_mutex_t m_mutex ;
-		
+        pthread_mutex_t m_mutex ;
+        
         friend class MyCondition ;
 
 };
