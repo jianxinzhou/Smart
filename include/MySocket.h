8130b31651b758b9169219675c46f9c4310f6971
KrisChou <zhoujx0219@163.com>
2014-10-20 20:50:19 +0800

    MySocket.h

--- include/MySocket.h
+++ include/MySocket.h
@@ -9,49 +9,60 @@
 #include <netinet/in.h>
 #include <iostream>
 #include "MyConf.h"
+
 class MySocket
 {
 	public:
-	MySocket( MyConf& conf):m_conf(conf)
+	MySocket(MyConf& conf)
+        :conf_(conf)
 	{
-		std::cout << m_conf.m_map["my_ip"] << std::endl ;
-		m_fd = socket(AF_INET, SOCK_DGRAM, 0);
-		if(m_fd == -1)
+		std::cout << conf_.mapConf_["my_ip"] << std::endl;
+		
+        peerfd_ = socket(AF_INET, SOCK_DGRAM, 0);
+		if(peerfd_ == -1)
 		{
 			std::cout <<__DATE__<<" "<<__TIME__ << " "<<__FILE__<<" "<<__LINE__<<": "<<"socket"<<std::endl ;
 			exit(-1);
 		}
-		m_len = sizeof(m_addr) ;
-		m_addr.sin_family = AF_INET ;
-		m_addr.sin_port = htons(atoi(m_conf.m_map["my_port"].c_str())) ;
-		m_addr.sin_addr.s_addr = inet_addr(m_conf.m_map["my_ip"].c_str());
-		if(-1 ==  bind(m_fd, (struct sockaddr*)&m_addr, sizeof(m_addr)) )
+		
+        addrLen_ = sizeof(addr_) ;
+		addr_.sin_family = AF_INET ;
+		addr_.sin_port = htons(atoi(conf_.mapConf_["my_port"].c_str())) ;
+		addr_.sin_addr.s_addr = inet_addr(conf_.mapConf_["my_ip"].c_str());
+		if(-1 ==  bind(peerfd_, (struct sockaddr*)&addr_, sizeof(addr_)) )
 		{
 			std::cout <<__DATE__<<" "<<__TIME__ << " "<<__FILE__<<" "<<__LINE__<<": "<<"bind"<<std::endl ;
 			exit(-1);
 		}
 	}
-	int send_message(void* buf, int len)
+	
+    int send_message(void* buf, int len)
 	{
-		return sendto(m_fd, buf, len, 0, (struct sockaddr*)&m_addr, sizeof(m_addr) );
+		return sendto(peerfd_, buf, len, 0, (struct sockaddr*)&addr_, sizeof(addr_) );
 	}
-	int recv_message(void* buf, int len)
+	
+    int recv_message(void* buf, int len)
 	{
 		std::cout <<"socket:: recv..." << std::endl ;
-		return recvfrom(m_fd, buf, len, 0, (struct sockaddr*)&m_addr, &m_len) ;
+		return recvfrom(peerfd_, buf, len, 0, (struct sockaddr*)&addr_, &addrLen_) ;
 	}
-	const struct sockaddr_in get_addr() const
+	
+    const struct sockaddr_in get_addr() const
 	{
-		return m_addr ;
+		return addr_ ;
 	}
-	~MySocket()
+	
+    ~MySocket()
 	{
-		close(m_fd);
+		close(peerfd_);
 	}
-	private:
-		int m_fd ;
-		struct sockaddr_in m_addr ;
-		socklen_t m_len ;
-		MyConf& m_conf ;
+	
+    private:
+		int peerfd_ ;               // 用于标识socket的描述符
+		struct sockaddr_in addr_ ;  // 用于保存服务器端或客户端ip和端口号信息
+		socklen_t addrLen_ ;        // 用于保存struct sockaddr_in 的长度
+		MyConf& conf_ ;             // 用户配置对象的引用
 };
+
+
 #endif
