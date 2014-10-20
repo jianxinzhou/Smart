cad402fb64d9c18e2f9361ffae41f253ea661f2b
KrisChou <zhoujx0219@163.com>
2014-10-20 21:51:10 +0800

    update

--- include/MyResult.h
+++ include/MyResult.h
@@ -4,9 +4,10 @@
 // 用来存储程序查询结果的，该对象放在优先级对象中
 class MyResult
 {
-	public:
-		std::string word_ ; // 保存词库中的词
-		int frequence_ ;    // 保存词库中词的词频
-		int distance_ ;     // 保存词库中的词与用户输入词的编辑距离
+    public:
+        std::string word_ ; // 保存词库中的词
+        int frequence_ ;    // 保存词库中词的词频
+        int distance_ ;     // 保存词库中的词与用户输入词的编辑距离
 };
+
 #endif
