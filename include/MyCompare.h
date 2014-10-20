657f60cb3e392e2cafa4492e9ec0e7d4d6b173a7
KrisChou <zhoujx0219@163.com>
2014-10-20 20:01:58 +0800

    MyCompare.h

--- include/MyCompare.h
+++ include/MyCompare.h
@@ -4,18 +4,26 @@
 class MyCompare
 {
 	public:
+
+        // 重载函数调用操作符，主要用于初始化优先级队列。
 		bool operator()(const MyResult& left, const MyResult& right)
 		{
 			if(left.m_dist > right.m_dist)
 			{
 				return true ;
-			}else if(left.m_dist == right.m_dist && left.m_frequence < right.m_frequence)
+			}
+            else if(left.m_dist == right.m_dist && 
+                    left.m_frequence < right.m_frequence)
 			{
 				return true ;
-			}else if(left.m_dist == right.m_dist && left.m_frequence == right.m_frequence && left.m_word > right.m_word)
+			}
+            else if(left.m_dist == right.m_dist && 
+                    left.m_frequence == right.m_frequence && 
+                    left.m_word > right.m_word)
 			{
 				return true ;
-			}else 
+			}
+            else 
 			{
 				return false ;
 			}
