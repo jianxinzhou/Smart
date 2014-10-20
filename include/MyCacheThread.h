617977da2719a968637ab15c13c022dbcab263bb
KrisChou <zhoujx0219@163.com>
2014-10-20 19:58:27 +0800

    finish MyCacheThread.h

--- include/MyCacheThread.h
+++ include/MyCacheThread.h
@@ -2,21 +2,28 @@
 #define __MYCACHETHREAD_H__
 #include "Thread.h"
 #include <vector>
-class ThreadPool ;
-class MyCache ;
-class MyThread ;
+class ThreadPool;
+class MyCache;
+class MyThread;
+
+
 class MyCacheThread	: public Thread
 {
-	public:
-		MyCacheThread(const int& num = 12):Thread(), m_pthreads(num)
-		{
 
-		}
-		void get_related(ThreadPool* p) ;
-	private:
+    public:
+		MyCacheThread(const int& num = 12)
+            : Thread(), vecWorkThreadPtr_(num)
+		{ }
+		
+        void get_related(ThreadPool* threadPoolPtr);
+	
+    private:
 		void run() ;
 		void scan_cache() ;
-		ThreadPool* m_p ;
-		std::vector<MyThread*> m_pthreads ;
+		
+        ThreadPool* threadPoolPtr_;                // 指向线程池的指针
+		std::vector<MyThread*> vecWorkThreadPtr_ ; // 含有指向工作线程指针的vector 。
 };
+
+
 #endif
